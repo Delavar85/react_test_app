7fc03f5183d56c562543918b47a387a997434952
Kostya <kosmat3101@gmail.com>
2022-08-16 23:05:05 +0300

    try do

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div>
+        <p className="pa">
+          5678
+        </p>
+      </div>
+      <div>
+        <p className="pb">
+1234
+        </p>
+      </div>
     </div>
   );
 }
